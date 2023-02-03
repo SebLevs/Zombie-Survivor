6130a3c55f98b7425ef0468c471876f57d69f71d
Sébastien <sebastien.levesque.slev@gmail.com>
2023-02-03 10:16:38 -0500

    feat: create player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Manager<Player>
+{
+    private void Start()
+    {
+        Debug.Log("Player started");
+    }
+}
