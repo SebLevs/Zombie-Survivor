e59f018e8493191f468b8f56ce83184f346e6798
Sébastien <sebastien.levesque.slev@gmail.com>
2023-02-19 16:39:15 -0500

    feat: set player as spawn points in circle
    still need debuging

--- Assets/Scripts/Location Getters/PositionGetter2DCircle.cs
+++ Assets/Scripts/Location Getters/PositionGetter2DCircle.cs
@@ -9,7 +9,7 @@ public class PositionGetter2DCircle : PositionGetter2D
     {
         Vector2 directionNormalized = Random.insideUnitCircle; // Unit vector in a circle
         Vector2 from = transform.position;
-        Vector2 directionWithModifier = from + directionNormalized * _radiusModifier;
+        Vector2 directionWithModifier = (from + directionNormalized) * _radiusModifier;
 
         return directionWithModifier;
     }
