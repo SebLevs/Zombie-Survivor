afab8ed8cbf87c827484489a94f75d5702da89b5
Sébastien <sebastien.levesque.slev@gmail.com>
2023-03-14 19:37:23 -0400

    chore: cleanup before new branch

--- Assets/Scripts/Factory/Enemy/AbstractEnemyFactory.cs
+++ Assets/Scripts/Factory/Enemy/AbstractEnemyFactory.cs
@@ -4,7 +4,7 @@ public abstract class AbstractEnemyFactory :ScriptableObject
 {
     [field:SerializeField] public string FactoryName { get; private set; }
 
-    public abstract Enemy CreateLowQuantityEnemy(UnityEngine.Vector3 position);
+    public abstract Enemy CreateLowQuantityEnemy(Vector3 position);
 
-    public abstract Enemy CreateHighQuantityEnemy(UnityEngine.Vector3 position);
+    public abstract Enemy CreateHighQuantityEnemy(Vector3 position);
 }
